b8419240cc67dd3cdb13d561b992a35b4c6885a2
VivekAlhat <vivekalhat14@outlook.com>
2020-08-22 16:06:16 +0530

    Render Posts

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const aboutContent =
-  "Pellentesque sodales, purus id suscipit vulputate, risus lectus porta mi, non hendrerit elit orci a justo. Nullam interdum arcu in leo aliquet viverra. Phasellus eget pretium eros. Fusce ante felis, egestas sit amet est vel, volutpat feugiat sapien. Maecenas vestibulum imperdiet gravida. Duis porttitor sagittis metus, non cursus nibh cursus in. Quisque tempor imperdiet eros eget consectetur.";
-const contactContent =
-  "Fusce leo leo, gravida vel luctus vel, imperdiet a ligula. Sed venenatis est ac quam faucibus consectetur. Nulla ipsum massa, porta ut ligula volutpat, elementum dapibus mi. Quisque tempor mollis mi nec malesuada. Nullam ut nisi dapibus, interdum ante suscipit, eleifend mauris. Mauris eget nibh magna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis sollicitudin aliquet lorem in gravida.";
 let posts = [];
 
 const app = express();
@@ -21,11 +17,11 @@ app.get("/", function (request, response) {
 });
 
 app.get("/about", function (request, response) {
-  response.render("about", { aboutContent: aboutContent });
+  response.render("about");
 });
 
 app.get("/contact", function (request, response) {
-  response.render("contact", { contactContent: contactContent });
+  response.render("contact");
 });
 
 app.get("/compose", function (request, response) {
@@ -38,5 +34,10 @@ app.post("/compose", function (request, response) {
     post: request.body.postData,
   };
   posts.push(post);
+  response.redirect("/");
+});
+
+app.get("/posts/:post", function (request, response) {
+  console.log(request.params.post);
   response.redirect("/");
 });
