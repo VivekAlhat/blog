7efba45b6c675b78b9243bf6f1c0ee73c9cbce8c
VivekAlhat <vivekalhat14@outlook.com>
2020-08-22 23:31:03 +0530

    Change Page Title Dynamically

--- app.js
+++ app.js
@@ -14,19 +14,19 @@ app.listen(3000, function () {
 });
 
 app.get("/", function (request, response) {
-  response.render("home", { posts: posts });
+  response.render("home", { posts: posts, page: "Blog" });
 });
 
 app.get("/about", function (request, response) {
-  response.render("about");
+  response.render("about", { page: "About" });
 });
 
 app.get("/contact", function (request, response) {
-  response.render("contact");
+  response.render("contact", { page: "Contact" });
 });
 
 app.get("/compose", function (request, response) {
-  response.render("compose");
+  response.render("compose", { page: "Compose" });
 });
 
 app.post("/compose", function (request, response) {
@@ -43,9 +43,7 @@ app.get("/posts/:post", function (request, response) {
   posts.forEach(function (p) {
     const h = _.lowerCase(p.title);
     if (h === pst) {
-      response.render("post", { title: p.title, post: p.post });
-    } else {
-      response.render("error");
+      response.render("post", { page: p.title, title: p.title, post: p.post });
     }
   });
 });
