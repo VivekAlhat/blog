687510a3b50a53990d95d20d31dad230037c9ea1
VivekAlhat <vivekalhat14@outlook.com>
2020-09-17 16:47:05 +0530

    Render From MongoDB

--- app.js
+++ app.js
@@ -1,20 +1,46 @@
+require("dotenv").config();
 const _ = require("lodash");
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
-let posts = [];
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Database setup
+mongoose.connect("mongodb://127.0.0.1:27017/blog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+const blogSchema = new mongoose.Schema({
+  pTitle: {
+    type: String,
+    required: true,
+  },
+  pContent: {
+    type: String,
+    required: true,
+  },
+});
+
+const Post = mongoose.model("Post", blogSchema);
+
 app.listen(3000, function () {
   console.log("Server is up and running");
 });
 
 app.get("/", function (request, response) {
-  response.render("home", { posts: posts, page: "Blog" });
+  Post.find(function (err, res) {
+    if (err) {
+      console.log(err);
+    } else {
+      response.render("home", { posts: res, page: "Blog" });
+    }
+  });
 });
 
 app.get("/about", function (request, response) {
@@ -30,20 +56,25 @@ app.get("/compose", function (request, response) {
 });
 
 app.post("/compose", function (request, response) {
-  const post = {
-    title: request.body.postTitle,
-    post: request.body.postData,
-  };
-  posts.push(post);
+  const p = new Post({
+    pTitle: request.body.postTitle,
+    pContent: request.body.postData,
+  });
+  p.save();
   response.redirect("/");
 });
 
-app.get("/posts/:post", function (request, response) {
-  const pst = _.lowerCase(request.params.post);
-  posts.forEach(function (p) {
-    const h = _.lowerCase(p.title);
-    if (h === pst) {
-      response.render("post", { page: p.title, title: p.title, post: p.post });
+app.get("/posts/:postid", function (request, response) {
+  const pst = request.params.postid;
+  Post.findOne({ _id: mongoose.Types.ObjectId(pst) }, function (err, res) {
+    if (!err) {
+      response.render("post", {
+        page: res.pTitle,
+        title: res.pTitle,
+        post: res.pContent,
+      });
+    } else {
+      console.log(err);
     }
   });
 });
